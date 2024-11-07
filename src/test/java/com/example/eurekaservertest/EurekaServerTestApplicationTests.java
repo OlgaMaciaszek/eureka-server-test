fea01b3bbb80878c6d7f8b8c52b7857d4223642e
Olga Maciaszek-Sharma <omaciaszeksh@vmware.com>
2024-11-07 12:47:14 +0100

    Init

---
+++ src/test/java/com/example/eurekaservertest/EurekaServerTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.eurekaservertest;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
